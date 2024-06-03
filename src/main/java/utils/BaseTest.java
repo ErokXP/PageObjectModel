9255dbd999a96b4fe09ab50772121ec16b35b6e7
Erok XP <cristian.bebesel@gmail.com>
2024-06-03 11:01:37 +0300

    curs25

--- src/main/java/utils/BaseTest.java
+++ src/main/java/utils/BaseTest.java
@@ -1,15 +1,25 @@
 package utils;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
+
+import com.google.common.io.Files;
 
 import pages.LoginPage;
 import pages.MenuPage;
@@ -39,12 +49,27 @@ public class BaseTest {
 		//driver.close();//inchide tabul curent
 	}
 	
-	
-	@BeforeMethod
-	public void initPageObjects() {
+
+	@AfterMethod
+	public void recordFailure(ITestResult result) {
 		
-		menu = new MenuPage(driver);
-		login = new LoginPage(driver);
+		if(ITestResult.FAILURE == result.getStatus()) {
+			TakesScreenshot sc = (TakesScreenshot) driver;
+			File poza = sc.getScreenshotAs(OutputType.FILE);
+			String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+			
+			try {
+				Files.copy(poza, new File("poze/"+result.getName()+"-"+timeStamp+".png"));
+				System.out.println("Picture saved!");
+			}catch(IOException e) {
+				System.out.println("Picture could not be saved!");
+				e.printStackTrace();
+			}
+			
+			
+		}
+		
+		
 		
 	}
 	
